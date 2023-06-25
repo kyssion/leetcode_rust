f838ce41300bd725861d19b8073d77b5042fef67
kyssion <1409915687@qq.com>
2023-06-25 14:48:27 +0800

    feat: code_0001

---
+++ src/leetcode/code1_100/code_001.cpp
@@ -0,0 +1,27 @@
+//
+// Created by kyssion on 23-6-25.
+//
+#include <bits/stdc++.h>
+using namespace  std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>hashtable;
+        for(int i=0;i<nums.size();i++){
+            auto it=hashtable.find(target-nums[i]);
+            if(it!=hashtable.end()){
+                return{it->second,i};
+            }
+            hashtable[nums[i]]=i;
+        }
+        return {};
+    }
+};
+int main(){
+    Solution sl;
+    vector<int> v = {1,2,3,4,5};
+    vector<int> ant = sl.twoSum(v,3);
+    std::for_each(ant.begin(), ant.end(), [](const auto &i){std::cout << i << " "; });
+    std::cout << "\n";
+    return 0;
+}
